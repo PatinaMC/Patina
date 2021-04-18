64ca74017ed6eef49b6c545e222492a2ffebd113
foss-mc <69294560+foss-mc@users.noreply.github.com>
2021-04-18 01:14:50 +0000

    rebrand url

--- rebrand.sh
+++ rebrand.sh
@@ -3,5 +3,6 @@ set -e
 name="Test"
 lowcasename="test"
 groupname="com.test"
-sed -i "s|com.ayff|$groupname|g;s|AYFF|$name|g;s|ayff|$lowcasename|g;" patches/server/0001-Modify-POM.patch patches/api/0001-Modify-POM.patch patches/server/0002-hardcode-BukkitVersion-1.16.5-R0.1-SNAPSHOT.patch settings.gradle.kts build.gradle.kts
+url="http://example.com"
+sed -i "s|com.ayff|$groupname|g;s|AYFF|$name|g;s|ayff|$lowcasename|g;s|https://github.com/foss-mc/A-Yatopia-Framwork-Fork|$url|g;" patches/server/0001-Modify-POM.patch patches/api/0001-Modify-POM.patch patches/server/0002-hardcode-BukkitVersion-1.16.5-R0.1-SNAPSHOT.patch settings.gradle.kts build.gradle.kts
 > README.md
