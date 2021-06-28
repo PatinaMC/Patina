dcc8a51c9ae0725986b6fe603c8ab825c1d9fd46
foss-mc <69294560+foss-mc@users.noreply.github.com>
2021-06-28 00:57:59 +0000

    Initial

--- LICENSE.md
+++
@@ -1,5 +0,0 @@
-All files from other projects keep their original license.
-
-All files in the buildSrc folder are licensed under [MIT](licenses/MIT.md).
-
-Patches are licensed under [GNU General Public License v3.0 or later](licenses/GPL.md).
--- rebrand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-name="Patina"
-lowcasename="patina"
-groupname="foss.patina"
-url="https://github.com/foss-mc/Patina"
-sed -i "s|com.ayff|$groupname|g;s|AYFF|$name|g;s|ayff|$lowcasename|g;s|https://github.com/foss-mc/A-Yatopia-Framwork-Fork|$url|g;" patches/server/0001-Modify-POM.patch patches/api/0001-Modify-POM.patch patches/server/0002-hardcode-BukkitVersion-1.16.5-R0.1-SNAPSHOT.patch settings.gradle.kts build.gradle.kts
-> README.md
