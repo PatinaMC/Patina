f6fe84abccd208548247c324247992a1d4cf7358
foss-mc <69294560+foss-mc@users.noreply.github.com>
2022-02-06 13:30:21 +0000

    update script

--- upstreamUtils/step1.js
+++ upstreamUtils/step1.js
@@ -4,7 +4,7 @@ const util = require('util');
 const fs = require('fs');
 
 function execAsync(cmd, callback) {
-  const c = child_process.exec(cmd, callback);
+  const c = child_process.exec(cmd, {shell: 'bash'}, callback);
 
   c.stdout.pipe(process.stdout);
   c.stderr.pipe(process.stderr);
