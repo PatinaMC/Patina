6f7f3408d829933c2711740f1a85128c5c3b50c6
foss-mc <69294560+foss-mc@users.noreply.github.com>
2022-02-08 10:24:24 +0000

    Update upstream

--- upstreamUtils/step2.sh
+++ upstreamUtils/step2.sh
@@ -10,11 +10,15 @@ rootProject.name(){
 }
 eval "$(grep '^rootProject.name =' settings.gradle.kts)"
 
-if [ -d "$name-Server" ]; then
-  cd "$name-Server"
-else
-  cd "$name-server"
-fi
+dirname="$(basename "$PWD")"
+
+for d in "$name-Server" "$name-server" "$dirname-Server" "$dirname-server"; do
+  if [ -d "$d" ]; then
+    serverd="$d"
+  fi
+done
+cd "$serverd"
+
 onecommit() {
 git reset --soft base
 git commit -m"$name $1 Changes
@@ -24,11 +28,13 @@ $(git log --format=%B --reverse HEAD..HEAD@{1})"
 }
 onecommit Server
 git format-patch --no-signature --zero-commit --full-index --no-stat -N -o .. HEAD^
-if [ -d "../$name-API" ]; then
-  cd "../$name-API"
-else
-  cd "../$name-api"
-fi
+cd ..
+for d in "$name-API" "$name-api" "$dirname-API" "$dirname-api"; do
+  if [ -d "$d" ]; then
+    apid="$d"
+  fi
+done
+cd "$apid"
 onecommit API
 git format-patch --no-signature --zero-commit --full-index --no-stat -N -o .. HEAD^
 cd ..
